e5dc2933bb8a01fe30c0e808a993af586dc1b403
yulppuma <yulppuma@gmail.com>
2017-02-02 14:52:52 -0800

    Added shake motion to magic 8-ball; still needs some tinkering to get sensitivty right

--- AndroidAppsJava/Magic_8-ball/app/src/main/java/com/example/yulpuma/magic_8_ball/MainActivity.java
+++ AndroidAppsJava/Magic_8-ball/app/src/main/java/com/example/yulpuma/magic_8_ball/MainActivity.java
@@ -1,5 +1,11 @@
 package com.example.yulpuma.magic_8_ball;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,21 +15,24 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
     //Not using
     public final static String EXTRA_MESSAGE = "com.example.magic_8_ball.MESSAGE";
     //String hello;
     Button mybtn;
     TextView txt;
-    String[] arr;
+    String[] arr = new String [20];
+    private SensorManager senSensorManager;
+    private Sensor senAccelerometer;
+    private long lastUpdate = 0;
+    private float last_x, last_y, last_z;
+    private static final int SHAKE_THRESHOLD = 400;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mybtn = (Button) findViewById(R.id.button_8);
-        txt = (TextView) findViewById(R.id.question);
-        arr = new String[20];
         arr[0] = "It is certain";
         arr[1] = "It is decidedly so";
         arr[2] = "Without a doubt";
@@ -44,6 +53,14 @@ public class MainActivity extends AppCompatActivity {
         arr[17] = "My sources say no";
         arr[18] = "Outlook not so good";
         arr[19] = "Very doubtful";
+        senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        senSensorManager.registerListener(this, senAccelerometer , SensorManager.SENSOR_DELAY_NORMAL);
+
+
+        mybtn = (Button) findViewById(R.id.button_8);
+        txt = (TextView) findViewById(R.id.question);
+
 
         mybtn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
@@ -52,5 +69,46 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Sensor mySensor = event.sensor;
+
+        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER){
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+
+            long curTime = System.currentTimeMillis();
+
+            if((curTime - lastUpdate) > 100){
+                long diffTime = (curTime - lastUpdate);
+                lastUpdate = curTime;
+
+                float speed = Math.abs(x + y + z - last_x - last_y - last_z)/ diffTime * 10000;
+                if (speed > SHAKE_THRESHOLD){
+                    txt = (TextView) findViewById(R.id.question);
+                    txt.setText(arr[(int) (Math.random()*20)]);
+                }
+                last_x = x;
+                last_y = y;
+                last_z = z;
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    protected void onPause() {
+        super.onPause();
+        senSensorManager.unregisterListener(this);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
 
 }
