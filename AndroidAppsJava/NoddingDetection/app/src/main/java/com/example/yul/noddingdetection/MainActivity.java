86b0f044fc8c38b3c49c90c8dd26625fec11dab0
Yul Puma <yulppuma@gmail.com>
2017-03-26 21:04:02 -0700

    Made some modificatons, but will probably just delete, remaking the class

--- AndroidAppsJava/NoddingDetection/app/src/main/java/com/example/yul/noddingdetection/MainActivity.java
+++ AndroidAppsJava/NoddingDetection/app/src/main/java/com/example/yul/noddingdetection/MainActivity.java
@@ -1,155 +1,44 @@
 package com.example.yul.noddingdetection;
 
-import android.content.Context;
+import android.app.Activity;
 import android.hardware.Camera;
-import android.hardware.Camera.FaceDetectionListener;
-import android.hardware.Camera.Face;
-import android.hardware.SensorManager;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.OrientationEventListener;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-import android.view.ViewGroup;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
 
-import java.util.List;
 
+public class MainActivity extends Activity {
+    private Camera mCamera = null;
+    private CameraView mCameraView = null;
 
-public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback{
-
-    public static final String TAG = CameraActivity.class.getSimpleName();
-
-    private Camera mCamera;
-
-    private int mOrientation;
-    private int mOrientationCompensation;
-    private OrientationEventListener mOrientationEventListener;
-
-    private int mDisplayRotation;
-    private int mDisplayOrientation;
-
-    private Camera.Face[] mFaces;
-
-    private SurfaceView mView;
-
-    private FaceOverlayView mFaceView;
-
-    private final CameraErrorCallback mErrorCallback = new Camera.ErrorCallback();
-
-    private FaceDetectionListener faceDetectionListener = new FaceDetectionListener() {
-        @Override
-        public void onFaceDetection(Face[] faces, Camera camera) {
-            Log.d("onFaceDetection", "Number of Faces:" + faces.length);
-            // Update the view now!
-            mFaceView.setFaces(faces);
-        }
-    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mView = new SurfaceView(this);
-        mFaceView = new FaceOverlayView(this);
-        addContentView(mFaceView, new ActionBar.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-        mOrientationEventListener = new SimpleOrientationEventListener(this);
-        mOrientationEventListener.enable();
-    }
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        SurfaceHolder holder = mView.getHolder();
-        holder.addCallback(this);
-    }
-
-    @Override
-    protected void onPause() {
-        mOrientationEventListener.disable();
-        super.onPause();
-    }
-
-    @Override
-    protected void onResume() {
-        mOrientationEventListener.enable();
-        super.onResume();
-    }
-
-    @Override
-    public void surfaceCreated(SurfaceHolder holder) {
-        mCamera = Camera.open();
-        mCamera.setFaceDetectionListener(faceDetectionListener);
-        mCamera.startFaceDetection();
         try{
-            mCamera.setPreviewDisplay(holder);
+            mCamera = Camera.open();//you can use open(int) to use different cameras
         } catch (Exception e){
-            Log.e(TAG, "Could not preview the img.", e);
-        }
-    }
-
-    @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-        if (holder.getSurface() == null) {
-            return;
-        }
-        // Try to stop the current preview:
-        try {
-            mCamera.stopPreview();
-        } catch (Exception e) {
-            // Ignore...
-        }
-        // Get the supported preview sizes:
-        Camera.Parameters parameters = mCamera.getParameters();
-        List<Camera.Size> previewSizes = parameters.getSupportedPreviewSizes();
-        Camera.Size previewSize = previewSizes.get(0);
-        // And set them:
-        parameters.setPreviewSize(previewSize.width, previewSize.height);
-        mCamera.setParameters(parameters);
-        // Now set the display orientation for the camera. Can we do this differently?
-        mDisplayRotation = Util.getDisplayRotation(CameraActivity.this);
-        mDisplayOrientation = Util.getDisplayOrientation(mDisplayRotation, 0);
-        mCamera.setDisplayOrientation(mDisplayOrientation);
-
-        if (mFaceView != null) {
-            mFaceView.setDisplayOrientation(mDisplayOrientation);
+            Log.d("ERROR", "Failed to get camera: " + e.getMessage());
         }
 
-        // Finally start the camera preview again:
-        mCamera.startPreview();
-    }
-
-    @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
-        mCamera.setPreviewCallback(null);
-        mCamera.setFaceDetectionListener(null);
-        mCamera.setErrorCallback(null);
-        mCamera.release();
-        mCamera = null;
-    }
-
-    private class SimpleOrientationEventListener extends OrientationEventListener {
-
-        public SimpleOrientationEventListener(Context context) {
-            super(context, SensorManager.SENSOR_DELAY_NORMAL);
+        if(mCamera != null) {
+            mCameraView = new CameraView(this, mCamera);//create a SurfaceView to show camera data
+            FrameLayout camera_view = (FrameLayout)findViewById(R.id.camera_view);
+            camera_view.addView(mCameraView);//add the SurfaceView to the layout
         }
 
-        @Override
-        public void onOrientationChanged(int orientation) {
-            // We keep the last known orientation. So if the user first orient
-            // the camera then point the camera to floor or sky, we still have
-            // the correct orientation.
-            if (orientation == ORIENTATION_UNKNOWN) return;
-            mOrientation = Util.roundOrientation(orientation, mOrientation);
-            // When the screen is unlocked, display rotation may change. Always
-            // calculate the up-to-date orientationCompensation.
-            int orientationCompensation = mOrientation
-                    + Util.getDisplayRotation(CameraActivity.this);
-            if (mOrientationCompensation != orientationCompensation) {
-                mOrientationCompensation = orientationCompensation;
-                mFaceView.setOrientation(mOrientationCompensation);
+        //btn to close the application
+        ImageButton imgClose = (ImageButton)findViewById(R.id.imgClose);
+        imgClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.exit(0);
             }
-        }
+        });
     }
 }
