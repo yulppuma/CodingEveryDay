10a7f1ad8397068d7e002c2c5a47461376882f2f
Yul Puma <yulppuma@gmail.com>
2017-03-26 22:39:19 -0700

    Added selfie camera use, still need face rec

---
+++ AndroidAppsJava/NoddingDetection/app/src/main/java/com/example/yul/noddingdetection/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.yul.noddingdetection;
+
+import android.app.Activity;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+
+
+public class MainActivity extends Activity {
+    private Camera mCamera = null;
+    private CameraView mCameraView = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        try{
+            int whichCamera = 0;
+            for(int i = 0; i < Camera.getNumberOfCameras(); i++){
+                Camera.getCameraInfo(i, new Camera.CameraInfo());
+                if(new Camera.CameraInfo().facing == Camera.CameraInfo.CAMERA_FACING_BACK)
+                    whichCamera = i;
+            }
+            mCamera = Camera.open(whichCamera);//you can use open(int) to use different cameras
+        } catch (Exception e){
+            Log.d("ERROR", "Failed to get camera: " + e.getMessage());
+        }
+
+        if(mCamera != null) {
+            mCameraView = new CameraView(this, mCamera);//create a SurfaceView to show camera data
+            FrameLayout camera_view = (FrameLayout)findViewById(R.id.camera_view);
+            camera_view.addView(mCameraView);//add the SurfaceView to the layout
+        }
+
+        //btn to close the application
+        ImageButton imgClose = (ImageButton)findViewById(R.id.imgClose);
+        imgClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.exit(0);
+            }
+        });
+    }
+}
